5b4388ba6da9e7e1999864f267ce50e604127475
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-02-22 20:52:22 +0000

    Fix things

--- codeserver/docker-entrypoint.sh
+++ codeserver/docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 source $HOME/.bashrc
-nvm use 14
 cd /ide
-yarn install
+#nvm exec 14 yarn install
 
 export CODE_SERVER_VERSION="4.0.2"
 export ARCH=`dpkg --print-architecture`
