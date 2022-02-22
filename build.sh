1fa6023757b91ffb288f3dbfd1c0471bc63ca1e1
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-02-22 06:21:48 +0000

    No amd64.

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #
-#   Note: This script does support amd64.
+#   Note: This script does support amd64, however there are some troubles with qemu.
 #
 
 set -e
 
 cd base
-docker buildx build --platform linux/arm64,linux/amd64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:$CI_COMMIT_REF_SLUG --push .
+docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:$CI_COMMIT_REF_SLUG --push .
 
 cd ../openvscode
-docker buildx build --platform linux/arm64,linux/amd64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:$CI_COMMIT_REF_SLUG --push .
+docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:$CI_COMMIT_REF_SLUG --push .
 
 cd ../codeserver
 docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver:$CI_COMMIT_REF_SLUG --push .
