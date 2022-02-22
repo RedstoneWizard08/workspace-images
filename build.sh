dc784ac9c6e2ffcc0486c9cee29fa7bdc6e4c45d
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-02-22 05:52:05 +0000

    Use tag for slug

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@
 set -e
 
 cd base
-docker buildx build --platform linux/arm64,linux/amd64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:$CI_COMMIT_REF_SLUG --push .
 
 cd ../openvscode
-docker buildx build --platform linux/arm64,linux/amd64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:$CI_COMMIT_REF_SLUG --push .
 
 cd ../codeserver
-docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver --push .
+docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver:$CI_COMMIT_REF_SLUG --push .
