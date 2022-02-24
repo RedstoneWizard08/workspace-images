5ee10b02d24f78a6c1b81c32a44b0d6c2df69c11
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-02-23 17:29:03 -0800

    Fix code-server and start headless support

--- build.sh
+++ build.sh
@@ -6,14 +6,45 @@
 
 set -e
 
+if [[ ! "$CI" == true ]]; then
+    TAG="local_devel"
+    ARGS="--build-arg TAG=local_devel"
+else
+    TAG="devel"
+    ARGS="--build-arg TAG=devel"
+
+    CI_MULTI_BASE_TAG="-t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:$CI_COMMIT_SHORT_SHA"
+    CI_MULTI_OPENVSCODE_TAG="-t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:$CI_COMMIT_SHORT_SHA"
+    CI_MULTI_CODESERVER_TAG="-t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver:$CI_COMMIT_SHORT_SHA"
+    CI_MULTI_THEIA_TAG="-t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/theia:$CI_COMMIT_SHORT_SHA"
+
+    CI_MULTI_HEADLESS_BASE_TAG="-t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/headless-base:$CI_COMMIT_SHORT_SHA"
+fi
+
+#
+#   Full images with IDE embedded in the container.
+#
+
+# Base Image
 cd base
-docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:$CI_COMMIT_SHORT_SHA -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:devel --push .
+docker buildx build --platform linux/amd64 $ARGS -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/base:$TAG $CI_MULTI_BASE_TAG --push .
 
+# OpenVSCode Image
 cd ../openvscode
-docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:$CI_COMMIT_SHORT_SHA -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:devel --push .
+docker buildx build --platform linux/amd64 $ARGS -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/openvscode:$TAG $CI_MULTI_OPENVSCODE_TAG --push .
 
+# Code-Server Image
 cd ../codeserver
-docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver:$CI_COMMIT_SHORT_SHA -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver:devel --push .
+docker buildx build --platform linux/amd64 $ARGS -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/codeserver:$TAG $CI_MULTI_CODESERVER_TAG --push .
 
+# Theia Image
 cd ../theia
-docker buildx build --platform linux/arm64 -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/theia:$CI_COMMIT_SHORT_SHA -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/theia:devel --push .
+docker buildx build --platform linux/amd64 $ARGS -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/theia:$TAG $CI_MULTI_THEIA_TAG --push .
+
+#
+#   Headless images without an IDE embedded in the container.
+#
+
+# Theia Image
+cd ../headless-base
+docker buildx build --platform linux/amd64 $ARGS -t reg.git.nosadnile.net:443/redstonewizard08/workspace-images/headless-base:$TAG $CI_MULTI_HEADLESS_BASE_TAG --push .
